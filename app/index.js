7faa002b233648e2094c26edead8669d049e058d
Diamondra Rasoamanana <diamondraras@gmail.com>
2019-01-30 21:32:00 +0300

    Aspirer page et archivation du resultat

---
+++ app/index.js
@@ -0,0 +1,25 @@
+const sendArchivedPage = require('./sendArchivedPage')
+const isUrl = require('is-url')
+const fs = require('fs')
+
+
+module.exports = (message, senderID, sendMessageData) => {
+    if (isUrl(message)) {
+        sendArchivedPage(message, function (zipFile) {
+            sendMessageData(senderID, {
+                    attachment: {
+                        type: "file",
+                        payload: {
+                            url: `${process.env.currentUrl}/${zipFile}`
+                        }
+                    }
+                }, function () {
+                    fs.unlinkSync(`./data/archives/${zipFile}`);
+                }
+
+            )
+        })
+    }else{
+        console.log("this is not a url")
+    }
+}
---
+++ routes/webhook.js
@@ -0,0 +1,63 @@
+const handleMessage = require('../app/index')
+const router = require('express').Router()
+const request = require('request')
+router.get('/', function (req, res) {
+    if (req.query['hub.verify_token'] === process.env.verifyToken) {
+        res.send(req.query['hub.challenge'])
+    } else {
+        res.send('Error, wrong token')
+    }
+})
+
+router.post('/', function (req, res) {
+    let messaging_events = req.body.entry[0].messaging
+
+    for (let i = 0; i < messaging_events.length; i++) {
+        let event = req.body.entry[0].messaging[i]
+        let sender = event.sender.id
+
+        if (event.message && event.message.text) {
+
+            let msg = event.message.text;
+            handleMessage(msg, sender, sendMessageData);
+
+        }
+        if (event.postback) {
+            let text = JSON.stringify(event.postback)
+            sendMessageData(sender, {
+                text: "Postback received: " + text.substring(0, 200)
+            })
+            continue
+        }
+
+    }
+    res.sendStatus(200)
+})
+
+function sendMessageData(sender, msgData, cb) {
+    const token = process.env.accessToken
+    request({
+        url: 'https://graph.facebook.com/v2.6/me/messages',
+        qs: {
+            access_token: token
+        },
+        method: 'POST',
+        json: {
+            recipient: {
+                id: sender
+            },
+            message: msgData,
+        }
+    }, function (error, response, body) {
+        if (cb) {
+            cb()
+        }
+        if (error) {
+            console.log('Error sending messages: ', error)
+        } else if (response.body.error) {
+            console.log('Error: ', response.body.error)
+        } else {
+        }
+    })
+}
+module.exports = router;
